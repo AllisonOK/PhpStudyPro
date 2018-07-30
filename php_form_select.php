c14bcf694bc65154f3ef732ab72707aa335e934f
1312999711@163.com <1312999711@163.com>
2018-07-30 17:13:03 +0800

    1

---
+++ php_form_select.php
@@ -0,0 +1,14 @@
+<?php
+//$q = isset()
+
+?>
+
+<form action="" method="get">
+    <select name="q">
+        <option value="">select a site</option>
+        <option value="RUNOOB">Runoob</option>
+        <option value="GOOGLE">Google</option>
+        <option value="TAOBAO">Taobao</option>
+    </select>
+    <input type="submit" value="select">
+</form>
